87a83b41a14a66e66b840457bc724949a4183746
gaolihai <1019933576@qq.com>
2021-12-14 00:58:54 +0800

    feature(cli): 实现对两标签数据集的训练脚本

---
+++ cli/train.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pathlib
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
+
+def main():
+    import globalConfig.config as config
+    dataDir = pathlib.Path(config.model2Classi.dataDir)
+    imgHeight = config.model2Classi.imgHeight
+    imgWidth = config.model2Classi.imgWidth
+    batchSize = config.model2Classi.batchSize
+    dataDir = pathlib.Path(config.model2Classi.dataDir)
+    epochs = config.model2Classi.epochs
+
+    trainDataset = tf.keras.utils.image_dataset_from_directory(
+        dataDir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(imgHeight, imgWidth),
+        batch_size=batchSize
+    )
+
+    valDataset = tf.keras.utils.image_dataset_from_directory(
+        dataDir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(imgHeight, imgWidth),
+        batch_size=batchSize
+    )
+
+    classNames = trainDataset.class_names
+
+    trainDataset = trainDataset.cache().shuffle(
+        1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+    valDataset = valDataset.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    from models.model2Classi.modelDefinition import model
+
+    print(model.summary())
+
+    history = model.fit(
+        trainDataset,
+        validation_data=valDataset,
+        epochs=epochs
+    )
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    modelConfig = config.model2Classi
+    if not os.path.exists(modelConfig.savedDir):
+        os.makedirs(modelConfig.savedDir)
+
+    model.save_weights(os.path.join(
+        modelConfig.savedDir, modelConfig.savedName))
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='training')
+    plt.plot(epochs_range, val_acc, label='validation')
+    plt.legend(loc='lower right')
+    plt.title('accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='training')
+    plt.plot(epochs_range, val_loss, label='validation')
+    plt.legend(loc='upper right')
+    plt.title('loss')
+    plt.show()
+
+
+main()
