da52691e3837c526528525c57ab4bb812635711c
gaolihai <1019933576@qq.com>
2021-12-14 00:58:32 +0800

    feature(cli): 实现用于预测的命令行工具

---
+++ cli/predict.py
@@ -0,0 +1,66 @@
+from enum import Enum
+import tensorflow
+from PIL import Image
+import numpy
+import argparse
+import os
+import sys
+import pandas
+
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
+
+def predict(model, imgPath, globalConfig):
+    # 如果 imgPath 是数组
+    if isinstance(imgPath, list):
+        return [predict(model, img, globalConfig) for img in imgPath]
+
+    img = numpy.array(
+        Image.open(imgPath)
+        .convert('RGB')
+        .resize((globalConfig.model2Classi.imgHeight, globalConfig.model2Classi.imgWidth))
+    )
+    img = tensorflow.expand_dims(img, 0)
+    return tensorflow.nn.softmax(model.predict(img))
+
+
+class Models(Enum):
+    Model2Classi = '2classi'
+    ModelMulClassi = 'mulclassi'
+
+
+def main():
+    models = [Models.Model2Classi, Models.ModelMulClassi]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Input image",
+                        required=True, type=str, nargs='+')
+    parser.add_argument("-m", "--model", help="Model", required=False,
+                        default='2classi', type=str, choices=models)
+
+    args = parser.parse_args()
+    import globalConfig.config as config
+    from models.model2Classi.modelDefinition import model as model2Classi
+    # from ..models.modelMulClassi.modelDefinition import model as modelMulClassi
+    if args.model == Models.Model2Classi.value:
+        model = model2Classi
+        model2Classi.load_weights(config.model2Classi.savedPath)
+    # elif args.model == Models.ModelMulClassi.value:
+    #     model = modelMulClassi
+    #     modelMulClassi.load_weights(config.modelMulClassi.savedPath)
+    else:
+        raise ValueError('Model not found')
+
+    scores = predict(model2Classi, args.input, config)
+
+    print()
+    for i, score in enumerate(scores):
+        print(f'{args.input[i]} probability: {score[0]}')
+        print(pandas.Series({classification: f'{str(float(score[0][i])*100)} %' for i, classification in enumerate(
+            config.model2Classi.classNames)}))
+        print(f'result: {config.model2Classi.classNames[numpy.argmax(score)]}')
+        print()
+
+
+main()
